d06c3d23295cea7dd48b83a7c46d30e787555c08
Viktorya <viktorya@shooga.ca>
2023-05-25 16:27:21 -0400

    add the content of the app in index.html

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isMac = process.platform === 'darwin';
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'ImageScalePro',
+        width: 500,
+        height: 600
+    });
+
+    //Show dev tools in in development
+    if(process.env.NODE_ENV !== 'production'){
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './app/index.html'));
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+});
+
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow()
+    }
+})
